9e6d090c850f98b90866bb4f506bf4e1045c5f47
Jannick Nielsen <jannicktrollenielsen@hotmail.com>
2022-08-23 10:39:43 +0200

    headeres

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 4000;
 
 const app = Express();
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 app.use(SongRouter);
 
 app.listen(process.env.PORT || port,() => {
